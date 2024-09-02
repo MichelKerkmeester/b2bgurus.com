94c5b1c1e82755b058390d8d9dd20f93deac3433
Michel Kerkmeester <82775228+MichelKerkmeester@users.noreply.github.com>
2024-09-02 09:13:29 +0200

    Update text-link.js

--- code/js/component/text-link.js
+++ code/js/component/text-link.js
@@ -1,27 +1,29 @@
 // Text link
 // Animated line on Hover
-$(document).ready(function () {
-  $(".btn--link-highlight").css({ width: "0%", visibility: "hidden" }); // Initially hide the line
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".btn--link-highlight").forEach(function (el) {
+    el.style.width = "0%";
+    el.style.visibility = "hidden"; // Initially hide the line
+  });
 
-  $(".btn--link").each(function () {
-    $(this).hover(
-      function () {
-        // Mouse enter animation
-        gsap.to($(this).find(".btn--link-highlight"), {
-          duration: 0.3,
-          width: "100%",
-          visibility: "visible",
-          ease: "power1.out",
-        });
-      },
-      function () {
-        // Mouse leave animation
-        gsap.to($(this).find(".btn--link-highlight"), {
-          duration: 0.3,
-          width: "0%",
-          ease: "power1.out",
-        });
-      }
-    );
+  document.querySelectorAll(".btn--link").forEach(function (link) {
+    link.addEventListener("mouseenter", function () {
+      // Mouse enter animation
+      gsap.to(link.querySelector(".btn--link-highlight"), {
+        duration: 0.3,
+        width: "100%",
+        visibility: "visible",
+        ease: "power1.out",
+      });
+    });
+
+    link.addEventListener("mouseleave", function () {
+      // Mouse leave animation
+      gsap.to(link.querySelector(".btn--link-highlight"), {
+        duration: 0.3,
+        width: "0%",
+        ease: "power1.out",
+      });
+    });
   });
 });
