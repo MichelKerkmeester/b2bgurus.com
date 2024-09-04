71fedb5b9efc96b99ecf6a79e2988beaa02b511a
Michel Kerkmeester <82775228+MichelKerkmeester@users.noreply.github.com>
2024-09-04 13:15:04 +0200

    Update parallax--process.js

--- code/js/gsap/parallax--process.js
+++ code/js/gsap/parallax--process.js
@@ -67,8 +67,8 @@ window.initProcessParallax = function () {
       };
 
       // Set initial states
-      gsap.set(heading, { y: "6.25rem", opacity: 0 });
-      gsap.set(description, { y: "9.375rem", opacity: 0 });
+      gsap.set(heading, { y: "6.25rem" });
+      gsap.set(description, { y: "9.375rem" });
       gsap.set(smallIllustration, {
         ...illustrationSettings[index].small,
         opacity: 0,
@@ -93,14 +93,12 @@ window.initProcessParallax = function () {
         // Animate heading
         gsap.to(heading, {
           y: 6.25 * (1 - progress) + "rem",
-          opacity: progress,
           duration: 0,
         });
 
         // Animate description
         gsap.to(description, {
           y: 9.375 * (1 - progress) + "rem",
-          opacity: progress,
           duration: 0,
         });
 
