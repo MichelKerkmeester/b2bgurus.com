0dc083a6eff857725d6bf264c9c18e256fcaa185
Michel Kerkmeester <82775228+MichelKerkmeester@users.noreply.github.com>
2024-10-06 08:57:37 +0200

    btn

--- code/js/component/text-link.js
+++ code/js/component/text-link.js
@@ -1,15 +1,15 @@
 // Text link
 // Animated line on Hover
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".btn--link-highlight").forEach(function (el) {
+  document.querySelectorAll(".btn--text-link-highlight").forEach(function (el) {
     el.style.width = "0%";
     el.style.visibility = "hidden"; // Initially hide the line
   });
 
-  document.querySelectorAll(".btn--link").forEach(function (link) {
+  document.querySelectorAll(".btn--text-link").forEach(function (link) {
     link.addEventListener("mouseenter", function () {
       // Mouse enter animation
-      gsap.to(link.querySelector(".btn--link-highlight"), {
+      gsap.to(link.querySelector(".btn--text-link-highlight"), {
         duration: 0.3,
         width: "100%",
         visibility: "visible",
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     link.addEventListener("mouseleave", function () {
       // Mouse leave animation
-      gsap.to(link.querySelector(".btn--link-highlight"), {
+      gsap.to(link.querySelector(".btn--text-link-highlight"), {
         duration: 0.3,
         width: "0%",
         ease: "power1.out",
